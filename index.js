729e3b8c55c4756bfe2c5f4b722c233aa2a07802
Jouni Jokelainen <jounijokelainen@gmail.com>
2020-06-17 20:35:10 +0300

    index.js edited

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("How are you doing");
+  res.send("Devops with Docker 2020 - task 3.2");
 });
 
-app.listen(8080, () => {
-  console.log("Listening on port 8080");
+app.listen(port, () => {
+  console.log("Server running..");
 });
